bb15c6353a0e2e57c3185d6c565c2e3ef77b34f4
vampire32 <moizabdul320@gmail.com>
2022-11-10 21:42:14 +0500

    backend

--- config/database.js
+++ config/database.js
@@ -1,16 +1,11 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'postgres'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', 'moiz'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-         
-        },
-        
-      },
-    });
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
